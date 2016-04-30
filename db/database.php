9a08bd7611d9a4339a7543a2d9048cad9547b986
Sebastian Babb <sebastianbabb@gmail.com>
2016-04-29 20:13:01 -0700

    Initial commit.

---
+++ db/database.php
@@ -0,0 +1,6 @@
+<?php
+/* Database credentials */
+define ("DB_USER", "PUT YOUR USER HERE");
+define ("DB_PASSWORD", "PUT YOUR PW HERE");
+define ("DB_DATABASE", "PUT YOUR DB HERE");
+define ("DB_HOST", "localhost");
